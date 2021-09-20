c2c8ae86b8f2c7c0efd1655ad9b1c4fea832b909
Luis Gustavo Macedo <luisgustavomacedo13@gmail.com>
2021-09-20 11:25:00 -0300

    'Header'

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @next/next/no-document-import-in-page
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
@@ -8,7 +9,7 @@ export default class MyDocument extends Document {
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
                     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet" />
-                    <title>Ignews</title>
+                    <link rel="shortcut icon" href="/favicon.png" type="image/png" />
                 </Head>
                 <body>
                     <Main />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import styles from '../styles/home.module.scss';
 
 export default function Home() {
   return (
@@ -7,9 +6,6 @@ export default function Home() {
     <Head>
       <title>Inicio | Ignews</title>
     </Head>
-      <h1 className={styles.title}>
-        Hello world
-      </h1>
     </>
   )
 }
