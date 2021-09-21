a5f070663f416451021bfb3b377a8fec6a5cf9c1
Luis Gustavo Macedo <luisgustavomacedo13@gmail.com>
2021-09-21 12:34:21 -0300

    Next-auth

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,47 @@
+import NextAuth from 'next-auth';
+import Providers from "next-auth/providers"
+
+import { query as q } from 'faunadb';
+import { fauna } from '../../../services/fauna';
+
+export default NextAuth({
+    providers: [
+        Providers.GitHub({
+            clientId: process.env.GITHUB_CLIENT_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET,
+            scope: 'read:user'
+        })
+    ],
+    callbacks: {
+        async signIn({ email }) {
+            try {
+                await fauna.query(
+                    q.If(
+                        q.Not(
+                            q.Exists(
+                                q.Match(
+                                    q.Index('user_by_email'),
+                                    q.Casefold(email)
+                                )
+                            )
+                        ),
+                        q.Create(
+                            q.Collection('users'),
+                            { data: { email } }
+                        ),
+                        q.Get(
+                            q.Match(
+                                q.Index('user_by_email'),
+                                q.Casefold(email)
+                            )
+                        )
+                    )
+                )
+    
+                return true;
+            } catch (e) {
+                return false;
+            }
+        }
+    }
+})
