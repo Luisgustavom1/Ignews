7f221648e639cdf8ba487fd0b3bc27f48e97e760
Luis Gustavo Macedo <luisgustavomacedo13@gmail.com>
2021-11-10 07:53:40 -0300

    fix the regex

--- src/components/ActiveLink/ActiveLink.spec.tsx
+++ src/components/ActiveLink/ActiveLink.spec.tsx
@@ -2,10 +2,22 @@ import { render } from '@testing-library/react';
 import React from 'react';
 import ActiveLink from '.';
 
+jest.mock('next/router', () => {
+    return {
+        useRouter() {
+            return {
+                asPath: '/'
+            }
+        }
+    }
+})
+
 test('Active link renders correctly', () => {
     const { debug } = render(
         <ActiveLink href='/' activeClassName='active'>
             <a>Home</a>
         </ActiveLink>
     )
+
+    debug()
 })
