02436332faabe914ce063e44765dcc4f64fb4323
Luis Gustavo Macedo <luisgustavomacedo13@gmail.com>
2021-11-10 07:46:32 -0300

    Initial configs jest

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,33 @@
 import React from 'react';
+import Link from 'next/link';
 
 import SignInButton from '../SignInButton'
 
 import styles from './styles.module.scss';
+import ActiveLink from '../ActiveLink';
 
 const Header: React.FC = () => {
   return(
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src='/images/logo.svg'/>
+        <img 
+          src='/images/logo.svg'
+          alt='Img Logo'
+          />
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <ActiveLink
+            href='/'
+            activeClassName={styles.active}
+          >
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink
+            href='/posts'
+            activeClassName={styles.active}
+            prefetch
+          >
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
         <SignInButton /> 
       </div>
--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -4,6 +4,7 @@ import styles from './styles.module.scss';
 import { signIn, useSession } from 'next-auth/client';
 import { getStripeJs } from '../../services/stripe-js';
 import { api } from '../../services/api';
+import { useRouter } from 'next/dist/client/router';
 
 interface SubscribeButtonProps {
   priceId: string
@@ -11,23 +12,29 @@ interface SubscribeButtonProps {
 
 const SubscribeButton: React.FC<SubscribeButtonProps> = ({ priceId }) => {
   const [session] = useSession();
+  const router = useRouter();
 
   async function handleSubscribe() {
     if(!session) {
       signIn('github');
       return;
-    } else {
-      try {
-        const response = await api.post('/subscribe');
+    }
 
-        const { sessionId } = response.data;
-        
-        const stripe = await getStripeJs();
+    if(session.activeSubscription) {
+      router.push('/posts');
+      return;
+    }
 
-        return stripe.redirectToCheckout({ sessionId });
-      } catch (e) {
-        alert(e.message)
-      }
+    try {
+      const response = await api.post('/subscribe');
+
+      const { sessionId } = response.data;
+      
+      const stripe = await getStripeJs();
+
+      return stripe.redirectToCheckout({ sessionId });
+    } catch (e) {
+      alert(e.message)
     }
   }
 
